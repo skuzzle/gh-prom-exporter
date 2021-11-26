99b332d0fc773b287ac18242118bb40d8cd8f625
Simon Taddiken <simon.taddiken@gmail.com>
2021-11-26 08:01:21 +0100

    Refactor meter construction from measurements

--- src/main/java/de/skuzzle/ghpromexporter/scrape/ScrapeService.java
+++ src/main/java/de/skuzzle/ghpromexporter/scrape/ScrapeService.java
@@ -9,8 +9,7 @@ import de.skuzzle.ghpromexporter.github.GitHubAuthentication;
 import de.skuzzle.ghpromexporter.github.ScrapableRepository;
 import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.Counter;
-import io.prometheus.client.SimpleCollector;
-import io.prometheus.client.SimpleCollector.Builder;
+import io.prometheus.client.Summary;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
@@ -34,48 +33,62 @@ class ScrapeService {
     }
 
     private RepositoryMetrics scrapeFresh(GitHubAuthentication authentication, ScrapeRepositoryRequest repository) {
-        final CollectorRegistry registry = new CollectorRegistry();
         final long start = System.currentTimeMillis();
-
+        final Meters meters = new Meters();
         return AppMetrics.scrapeDuration().record(() -> {
             final var repositoryFullName = repository.repositoryFullName();
             final var scrapableRepository = ScrapableRepository.load(authentication, repositoryFullName);
-            b(Counter.build("additions", "TBD"), registry)
-                    .labels(repository.owner(), repository.name())
-                    .inc(scrapableRepository.totalAdditions());
-            b(Counter.build("deletions", "TBD"), registry)
-                    .labels(repository.owner(), repository.name())
-                    .inc(scrapableRepository.totalDeletions());
-            b(Counter.build("stargazers", "The repository's stargazer count"), registry)
-                    .labels(repository.owner(), repository.name())
-                    .inc(scrapableRepository.stargazersCount());
-            b(Counter.build("forks", "The repository's fork count"), registry)
-                    .labels(repository.owner(), repository.name())
-                    .inc(scrapableRepository.forkCount());
-            b(Counter.build("open_issues", "The repository's open issue count"), registry)
-                    .labels(repository.owner(), repository.name())
-                    .inc(scrapableRepository.openIssueCount());
-            b(Counter.build("subscribers", "The repository's subscriber count"), registry)
-                    .labels(repository.owner(), repository.name())
-                    .inc(scrapableRepository.subscriberCount());
-            b(Counter.build("watchers", "The repository's watcher count"), registry)
-                    .labels(repository.owner(), repository.name())
-                    .inc(scrapableRepository.watchersCount());
-            b(Counter.build("size", "The repository's size in KB"), registry)
-                    .labels(repository.owner(), repository.name())
-                    .inc(scrapableRepository.size());
 
+            meters.additions.labels(repository.owner(), repository.name()).inc(scrapableRepository.totalAdditions());
+            meters.deletions.labels(repository.owner(), repository.name()).inc(scrapableRepository.totalDeletions());
+            meters.stargazers.labels(repository.owner(), repository.name()).inc(scrapableRepository.stargazersCount());
+            meters.forks.labels(repository.owner(), repository.name()).inc(scrapableRepository.forkCount());
+            meters.open_issues.labels(repository.owner(), repository.name()).inc(scrapableRepository.openIssueCount());
+            meters.subscribers.labels(repository.owner(), repository.name()).inc(scrapableRepository.subscriberCount());
+            meters.watchers.labels(repository.owner(), repository.name()).inc(scrapableRepository.watchersCount());
+            meters.size.labels(repository.owner(), repository.name()).inc(scrapableRepository.size());
             final long scrapeDuration = System.currentTimeMillis() - start;
-            final RepositoryMetrics metrics = RepositoryMetrics.fresh(repository, registry, scrapeDuration);
+            meters.scrapeDuration.labels(repository.owner(), repository.name()).observe(scrapeDuration);
+
+            final RepositoryMetrics metrics = RepositoryMetrics.fresh(repository, meters.registry, scrapeDuration);
             log.debug("Scraped fresh metrics for {} in {}ms", repository, scrapeDuration);
             return metrics;
         });
     }
 
-    private <B extends Builder<B, C>, C extends SimpleCollector<?>> C b(
-            SimpleCollector.Builder<B, C> b,
-            CollectorRegistry registry) {
-        return b.namespace(NAMESPACE).labelNames(LABEL_OWNER, LABEL_REPOSITORY).register(registry);
-    }
+    private static class Meters {
+        private final CollectorRegistry registry;
+        private final Counter additions;
+        private final Counter deletions;
+        private final Counter stargazers;
+        private final Counter forks;
+        private final Counter open_issues;
+        private final Counter subscribers;
+        private final Counter watchers;
+        private final Counter size;
+        private final Summary scrapeDuration;
 
+        public Meters() {
+            this.registry = new CollectorRegistry();
+            this.additions = Counter.build("additions", "TBD")
+                    .namespace(NAMESPACE).labelNames(LABEL_OWNER, LABEL_REPOSITORY).register(registry);
+            this.deletions = Counter.build("deletions", "TBD")
+                    .namespace(NAMESPACE).labelNames(LABEL_OWNER, LABEL_REPOSITORY).register(registry);
+            this.stargazers = Counter.build("stargazers", "The repository's stargazer count")
+                    .namespace(NAMESPACE).labelNames(LABEL_OWNER, LABEL_REPOSITORY).register(registry);
+            this.forks = Counter.build("forks", "The repository's fork count")
+                    .namespace(NAMESPACE).labelNames(LABEL_OWNER, LABEL_REPOSITORY).register(registry);
+            this.open_issues = Counter.build("open_issues", "The repository's open issue count")
+                    .namespace(NAMESPACE).labelNames(LABEL_OWNER, LABEL_REPOSITORY).register(registry);
+            this.subscribers = Counter.build("subscribers", "The repository's subscriber count")
+                    .namespace(NAMESPACE).labelNames(LABEL_OWNER, LABEL_REPOSITORY).register(registry);
+            this.watchers = Counter.build("watchers", "The repository's watcher count")
+                    .namespace(NAMESPACE).labelNames(LABEL_OWNER, LABEL_REPOSITORY).register(registry);
+            this.size = Counter.build("size", "The repository's size in KB")
+                    .namespace(NAMESPACE).labelNames(LABEL_OWNER, LABEL_REPOSITORY).register(registry);
+            this.scrapeDuration = Summary.build("scrape_duration", "Duration of a single scrape")
+                    .namespace(NAMESPACE).labelNames(LABEL_OWNER, LABEL_REPOSITORY).register(registry);
+        }
+
+    }
 }
